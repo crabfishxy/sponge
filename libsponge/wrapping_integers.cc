8acaa9f9c2e686740bc53e8867f75a4e30c4f8fc
crabfish <lhxxxy43@gmail.com>
2021-04-24 13:42:49 +0800

    finish wrapping_integers

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,4 +1,5 @@
 #include "wrapping_integers.hh"
+#include <cstdint>
 
 // Dummy implementation of a 32-bit wrapping integer
 
@@ -14,8 +15,10 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t temp = isn.raw_value() + n % (uint64_t(uint64_t(UINT32_MAX))+1);
+    uint32_t real_v = uint32_t(temp % (uint64_t(UINT32_MAX)+1));
+
+    return WrappingInt32{real_v};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +32,16 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t abs_seq = (n-isn) % (uint64_t(UINT32_MAX)+1);
+    uint64_t abs_diff = checkpoint % (uint64_t(UINT32_MAX)+1);
+    uint64_t lower_bound = abs_seq;
+    if (checkpoint >= (uint64_t(UINT32_MAX)+1)) {
+        lower_bound = checkpoint - abs_diff - (uint64_t(UINT32_MAX)+1) + abs_seq;
+    }
+    uint64_t middle_bound = checkpoint - abs_diff + abs_seq;
+    uint64_t upper_bound = checkpoint - abs_diff +  (uint64_t(UINT32_MAX)+1) + abs_seq;
+    uint64_t lower_diff = min(checkpoint - lower_bound, lower_bound - checkpoint);
+    uint64_t middle_diff = min(checkpoint - middle_bound, middle_bound-checkpoint);
+    uint64_t upper_diff = min(checkpoint - upper_bound, upper_bound-checkpoint);
+    return lower_diff < upper_diff ? (lower_diff < middle_diff?lower_bound:middle_bound):(middle_diff < upper_diff?middle_bound:upper_bound);
 }
